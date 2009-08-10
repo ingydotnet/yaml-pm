ff54fe545709d343b45e2662f55b87e0aed3bbfa
Adam Kennedy <adamk@cpan.org>
2009-08-10 03:29:55 +0000

    Adding 2009 copyright statement

--- lib/YAML.pm
+++ lib/YAML.pm
@@ -805,6 +805,8 @@ Copyright (c) 2005, 2006, 2008. Ingy döt Net.
 
 Copyright (c) 2001, 2002, 2005. Brian Ingerson.
 
+Some parts copyright (c) 2009 Adam Kennedy
+
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
