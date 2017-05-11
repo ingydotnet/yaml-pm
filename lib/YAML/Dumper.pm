57a885f466a6877a7001e59a1fa9a044877b574b
Tina Müller <cpan2@tinita.de>
2017-05-11 18:25:30 +0200

    Add error to YAML::Dumper

---
+++ lib/YAML/Dumper.pm
@@ -0,0 +1,14 @@
+die "YAML::Dumper has been moved to YAML::Old::Dumper";
+
+__END__
+=pod
+
+=head1 NAME
+
+YAML::Dumper - Deprecated
+
+=head1 DESCRIPTION
+
+The original YAML.pm distribution has been replaced by L<YAML::Old>.
+
+=cut
