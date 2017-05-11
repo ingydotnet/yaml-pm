68c0f2101f461b5f7ee3ac6e0d879a3cae116611
Tina Müller <cpan2@tinita.de>
2017-05-11 19:16:47 +0200

    Implement DumpFile/LoadFile; handle import args

--- lib/YAML.pm
+++ lib/YAML.pm
@@ -3,18 +3,20 @@ use strict;
 use warnings;
 
 use base 'Exporter';
-our @EXPORT = qw(Load Dump);
+our @EXPORT = qw( Load Dump );
+our @EXPORT_OK = qw( LoadFile DumpFile );
 
 our $VERSION = '1.23_001';
 
 sub import {
-    my $package = shift;
-    if (not @_) {
-        $package->export_to_level(1, @_);
+    my ($package, @args) = @_;
+    for my $arg (@args) {
+        if ($arg !~ /^(Load|LoadFile|Dump|DumpFile)$/) {
+            # Handle new API; return;
+            die __PACKAGE__.':'.__LINE__.": import @_\n";
+        }
     }
-    else {
-        warn __PACKAGE__.':'.__LINE__.": import @_\n";
-    }
+    $package->export_to_level(1, @args);
 }
 
 sub Load {
@@ -25,6 +27,16 @@ sub Load {
 sub Dump {
     require YAML::Old;
     goto &YAML::Old::Dump;
+}
+
+sub LoadFile {
+    require YAML::Old;
+    goto &YAML::Old::LoadFile;
+}
+
+sub DumpFile {
+    require YAML::Old;
+    goto &YAML::Old::DumpFile;
 }
 
 1;
